e4cccc841380d133c11236a2dcd089df735f9c25
Seth Helfgott <sethleland@gmail.com>
2015-06-03 23:27:45 +0000

    Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Seth Helfgott"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Seth Helfgott"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Seth Helfgott"
   end
 
 end
